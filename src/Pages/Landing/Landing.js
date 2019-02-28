2363949952c9f8a2b564ed55d479cad5c9799238
Dallin-H <dallinhyde@gmail.com>
2019-02-28 16:32:58 -0700

    css updates

--- src/Pages/Landing/Landing.js
+++ src/Pages/Landing/Landing.js
@@ -12,23 +12,49 @@ function Landing() {
         location2="/login"
       />
       <div className="Body__Container__Landing">
-        <div className="Box__Landing">
-          <div className="TextBox__Landing">
-            Welcome! To get started, please create an account
+        <div className="Desktop__View">
+          <div className="Box__Landing__Intro">
+            <div className="TextBox__Landing__Intro">
+              Welcome! To get started, please create an account
+            </div>
+          </div>
+          <div className="Desktop__Family">
+            <div className="Box__Landing">
+              <div className="TextBox__Landing">Create Decks</div>
+            </div>
+
+            <div className="Box__Landing">
+              <div className="TextBox__Landing">
+                Create cards for the deck, with one correct answer, and three
+                wrong answers
+              </div>
+            </div>
+
+            <div className="Box__Landing">
+              <div className="TextBox__Landing">Start practicing!</div>
+            </div>
           </div>
         </div>
-        <div className="Box__Landing">
-          <div className="TextBox__Landing">Create Decks</div>
-        </div>
-        <div className="Box__Landing">
-          <div className="TextBox__Landing">
-            Create cards for the deck, with one correct answer, and three
-            wrong answers
+
+        <div className="Mobile__View">
+          <div className="Box__Landing">
+            <div className="TextBox__Landing">
+              Welcome! To get started, please create an account
+            </div>
           </div>
-        </div>
-        <div className="Box__Landing">
-          <div className="TextBox__Landing">
-            Start practicing!
+          <div className="Box__Landing">
+            <div className="TextBox__Landing">Create Decks</div>
+          </div>
+
+          <div className="Box__Landing">
+            <div className="TextBox__Landing">
+              Create cards for the deck, with one correct answer, and three
+              wrong answers
+            </div>
+          </div>
+
+          <div className="Box__Landing">
+            <div className="TextBox__Landing">Start practicing!</div>
           </div>
         </div>
       </div>
